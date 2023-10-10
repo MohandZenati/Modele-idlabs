286ab9ddef30cdfdc31038a159b7eeee954131d1
MohandZenati <81512623+MohandZenati@users.noreply.github.com>
2023-10-10 20:17:47 +0200

    change weight

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="cardlist">
         <div class="header__cardlist">
-            <span>Showing <strong>127</strong>Jobs</span>
+            <span>Showing  <strong>127</strong> Jobs</span>
             <div class="sort">
                 <span>Sort by :</span>
                 <span>Newest </span>
@@ -37,7 +37,7 @@
                 <button>12</button>
                 <img class="arrow__pagination" src="../assets/i-arrow.svg" alt="">
         </div>
-        
+
     </div>
 </template>
 
--- src/components/FilterList.vue
+++ src/components/FilterList.vue
@@ -1,35 +1,34 @@
 <template>
     <div class="filterbar">
-      <div class="filterbar__container">
-        <Filter />
-      </div>
-    </div>
-  
-    <div class="btn__filterbar__container">
-      <button @click="toggleFilter" class="btn__filter">
-        <img src="../assets/i-filter.svg" alt="">
-        <span>Filters</span>
-      </button>
-  
-      <div class="btn__filterbar__content">
-        <div  @click="toggleFilter" class="btn__filterbar__header">
-          <img src="../assets/i-arrow.svg" alt="">
-          <span>Retour</span>
-        </div>   
         <div class="filterbar__container">
-          <Filter />
-          <button>Apply Filters</button>
+            <Filter />
         </div>
-      </div>
     </div>
-  </template>
+
+    <div class="btn__filterbar__container">
+        <button @click="toggleFilter" class="btn__filter">
+            <img src="../assets/i-filter.svg" alt="">
+            <span>Filters</span>
+        </button>
+
+        <div class="btn__filterbar__content">
+            <div @click="toggleFilter" class="btn__filterbar__header">
+                <img src="../assets/i-arrow.svg" alt="">
+                <span>Retour</span>
+            </div>
+            <div class="filterbar__container">
+                <Filter />
+                <button>Apply Filters</button>
+            </div>
+        </div>
+    </div>
+</template>
   
-  <script setup>
-  import Filter from '../components/Filter.vue';
-  
-  function toggleFilter() {
+<script setup>
+import Filter from '../components/Filter.vue';
+function toggleFilter() {
     let popup = document.querySelector(".btn__filterbar__content");
     popup.classList.toggle("open");
-  }
-  </script>
+}
+</script>
   
